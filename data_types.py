6be19b6812d44f7969a680e4a98104c13ebd39b2
Rkv1650 <rkvgaming9@gmail.com>
2023-05-29 08:52:16 +0530

    k

---
+++ data_types.py
@@ -0,0 +1,20 @@
+# Numeric   >               Int,  flot, complex
+# text type >               string
+# sequence type >           list,tuple ,range
+# mapping type >            dict
+# Set Type >                Set frozenset
+# Booleam Type >            bytes ,bytearry memoryview
+# none >                    None
+ 
+#  Numeric   >               Int
+a=10
+print(a)
+print(type(a))
+# Numeric   >               flot
+a=10.1
+print(a)
+print(type(a))
+# Numeric   >               Complex Number
+a=10j
+print(a)
+print(type(a))
