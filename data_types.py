8621a9c1572d68c57bf6d40233f4ec3617bb7595
Rkv1650 <rkvgaming9@gmail.com>
2023-05-29 09:26:37 +0530

    g

--- data_types.py
+++ data_types.py
@@ -29,5 +29,26 @@ print(type(a))
 # 1 List 
 a=['Rahul', 'ram' , 'Devi']
 print(type(a))
+# 2 Tuple
 a=(1,2,3,4,5,6,7,8,9,10)
 print(type(a))
+# 3 Range
+a=range(6,8)
+print(type(a))
+# Maping Data Tyape
+# 1 Dict
+a = {"name" : "John", "age" : 36}
+print(type(a))
+# Set Types Data
+# 1  Set
+a={"apple", "banana", "cherry"}
+print(type(a))
+    # Frozenset
+a=x = frozenset({"apple", "banana", "cherry"})
+print(type(a))
+# Boolean Data Types
+a=True
+print(type(a))
+# Noe
+a=None
+print(a)
