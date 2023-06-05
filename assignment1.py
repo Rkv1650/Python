25597f720ae302a791d7a044725e8146b481247e
Rkv1650 <rkvgaming9@gmail.com>
2023-06-05 15:44:03 +0530

    ji

---
+++ assignment1.py
@@ -0,0 +1,38 @@
+# Q1 --> Write a program to print "Heloo Python"
+# Answer
+print("Hello Python")
+# Q2 --> Write a program to store one value in virable and print that virable
+# Answer
+age=23
+print(age)
+# Q3 --> Write a program to take value from use input and print that value
+# Answer
+enter_your_name=input("Enter Your Name  ") 
+print(enter_your_name)
+
+# Q4 --> Write a program to take two value from user input  print sum division less
+# Answer
+value_1=int(input("Enter your First Value "))
+value_2=int(input("Enter your 2nd Value "))
+total= value_1+value_2
+sub= value_1-value_2
+div= value_1/value_2
+
+print(total)
+print(sub)
+print(div)
+#Q5 print multiple Values
+a=("ram",
+   1,
+   1.0)
+print(a)
+
+# Q6 --> Write a program  to Find a typeof varibles
+# Answer
+a=1
+print(type(a))
+a=1.0
+print(type(a))
+a=1-5j
+print(type(a))
+
