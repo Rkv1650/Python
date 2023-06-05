bb9cb57a52dee76218dbf39c8fa6bf88752a324d
Rkv1650 <rkvgaming9@gmail.com>
2023-06-05 14:43:48 +0530

    kl

---
+++ varibles.py
@@ -0,0 +1,20 @@
+# var->variables are a like Container where store a data by user by user# Rules-> A variable name must start with a letter or the underscore character
+
+# -> A variable name cannot start with a number
+# -> A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+# -> Variable names are case-sensitive (age, Age and AGE are three different variables)
+
+
+# Ex
+age=24
+a_1=23
+aBc=56
+_my_var=12
+z=age+a_1+aBc+_my_var
+print(z)
+
+
+# Type Conversion  --->>one data to any other data
+a=1
+b=float(a)
+print(type(b))
